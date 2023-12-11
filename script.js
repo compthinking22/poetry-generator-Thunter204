50a0697c506cf0444fb705329f52ed4b2182c938
Thunter204 <128633716+Thunter204@users.noreply.github.com>
2023-12-11 11:37:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,42 +2,42 @@
 
 /*This code basically breaks the words in the corpus down using Rita, analyzes the text to see if 
 it's a noun, verb, adjective, r? and then stores it in the words dictionary*/
-/*for my project, I want to create a a recipe title, an ingredient list, and then (maybe) dicrections 
-  for the recipe.*/
 /*seperate text file for titles, ingredients. var words shoul dbe changed to two dictionaries --> title dict and ingredient dict.*/
 var result;
 var grammar;
 
+
 var grammarDict = {
-  "start": "$n #v $p $sec\n\n $num $a $v\n $num $a $sec\n $num $a $r\n $num $a $r\n $num $a $r\n $num $a $r\n",
-  "#v": "Chicken | Garlic Chicken | Fried Chicken | Chili | Pulled Pork | Beef Stew | boneless chicken thighs | Beef | Stir Fry",
+  "start": "$n $fir $p $sec\n\n $num $a $fir\n $num $a $sec\n $num $a $r\n $num $a $r\n $num $a $r\n $num $a $r\n", //title, recipe
   "p": "with | with a side of ",
   "num": "1 | 2 | 3 | 4",
-  "#sec":"$r"
+  "#fir":"$v",
+  "#sec":"$r" //Gets repeated in title and once in the recipe
 };
 
 var words = {
   'n': "", //titles
   'v': "", 
   'a': "", //Measurements
-  'r': "", //ingredients
+  'r': "" //ingredients
 };
 
 function preload() {
   result = loadStrings('TITLES.txt');
   resultTwo = loadStrings('INGREDIENTS.txt');
   resultThree = loadStrings('MEASURMENTS.txt');
+  resultFour = loadStrings('Mingre.txt');
 
 }
 
 function setup() {
-  createCanvas(1920, 923); 
+  createCanvas(1920, 923); //size of my screen
   noLoop(); // Stop the draw loop after the first iteration
 
   const fullString = result.join("\n");
   const fullERString = resultTwo.join("\n");
   const fullESTString = resultThree.join("\n");
-  //const fullFString = resultFour.join("\n");
+  const fullFString = resultFour.join("\n");
 
   /*-------------------------------TITLES------------------------------------------------ */
 
@@ -84,6 +84,20 @@ function setup() {
   });
 
   grammarDict['a'] = words['a'];
+  grammar = RiTa.grammar(grammarDict);
+
+  fullFString.split('\n').forEach((line) => { 
+    const lineWords = [];
+    RiTa.tokenize(line).forEach((element) => {
+      var pos = RiTa.pos(element, { simple: true });
+      if ((pos == 'n') || (pos == 'v') || (pos == 'a') || pos == 'r') {
+        lineWords.push(element);
+      }
+    });
+    words['v'] += " | " + lineWords.join(" ");
+  });
+
+  grammarDict['v'] = words['v'];
   grammar = RiTa.grammar(grammarDict);
 
 
